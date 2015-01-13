d5a0f158bdcc148ba222b9d00b2efd39648cef34
Josh Alexy <jalexy12@gmail.com>
2015-01-13 16:39:14 -0500

    First commit of the blog

---
+++ blog.rb
@@ -0,0 +1,22 @@
+#Blog Class
+#		front_page
+#Post Class
+# title
+# date
+# text
+# post needs to be printed like
+
+class Blog
+	def front_page
+		puts "This is my blog"
+	end
+
+end
+
+	blog = Blog.new
+	blog.front_page
+
+
+
+
+
