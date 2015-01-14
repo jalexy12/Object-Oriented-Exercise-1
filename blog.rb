fb2c994faac47d0d2b5c77a318f7b602ea1479e5
Josh Alexy <jalexy12@gmail.com>
2015-01-14 09:04:41 -0500

    Final blog entry

--- blog.rb
+++ blog.rb
@@ -5,16 +5,90 @@
 # date
 # text
 # post needs to be printed like
+#Post title 1
+#**************
+#Post 1 text
+#----------------
+#Post title 2
+#**************
+#vehicles = [Bus.new(8, "buuuuhhh"), Car.new(4,"Honk"), Car.new(3,"VROOOM"), Bus.new(20, "...")]
 
 class Blog
-	def front_page
-		puts "This is my blog"
+
+	def initialize()
+		@posts = []	
+
 	end
+
+	def add_post(newpost)
+		
+		@posts.push(newpost)
+	end
+
+	
+
+	def print_page
+		puts @posts
+		
+	end
+
+
+
 
 end
 
-	blog = Blog.new
-	blog.front_page
+class Post
+
+
+	def initialize(title, date, text)
+		@title = title
+		@date = date
+		@text = text
+	end
+	def to_s
+		return "#{@title}\n**************************\nDate: #{@date}\nText: #{@text}\n---------------------------------"
+	end
+
+
+end
+
+
+
+myBlog =  Blog.new
+post1 = Post.new("Cool Title", "12/22/14", "Cool texttttttttttttttttttttttt")
+post2 = Post.new("Sweet Title", "12/23/14", "Sweeeeetttt texxxttttt")
+post3 = Post.new("Awesome Title", "12/24/14", "Awesome texxxxxt")
+
+
+myBlog.add_post(post1)
+myBlog.add_post(post2)
+myBlog.add_post(post3)
+myBlog.print_page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
